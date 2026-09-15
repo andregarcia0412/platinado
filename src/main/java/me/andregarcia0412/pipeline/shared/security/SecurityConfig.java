779374a6d6aaa92cfc1b93cf12de8f10bb465803
andregarcia0412 <andregarcia0412@gmail.com>
2026-09-15 16:44:21 -0300

    :sparkles: feature: setup cors config

--- src/main/java/me/andregarcia0412/pipeline/shared/security/SecurityConfig.java
+++ src/main/java/me/andregarcia0412/pipeline/shared/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +13,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 ).csrf(csrf -> csrf.disable());
