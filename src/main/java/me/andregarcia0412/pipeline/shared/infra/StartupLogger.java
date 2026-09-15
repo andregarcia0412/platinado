0fff21c832312c9dfe1f8406966dbe0a74a547b4
andregarcia0412 <andregarcia0412@gmail.com>
2026-09-15 17:12:32 -0300

    :wrench: chore: setup startup logger

---
+++ src/main/java/me/andregarcia0412/pipeline/shared/infra/StartupLogger.java
@@ -0,0 +1,24 @@
+package me.andregarcia0412.pipeline.shared.infra;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartupLogger {
+    private static final Logger logger = LoggerFactory.getLogger(StartupLogger.class);
+
+    @Value("${server.port}")
+    private String port;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onReady() {
+        logger.info("\uD83D\uDE80 Server running on port {}", port);
+        logger.info("\uD83D\uDCDD Swagger UI docs on: http://localhost:{}/swagger-ui/index.html", port);
+    }
+
+
+}
