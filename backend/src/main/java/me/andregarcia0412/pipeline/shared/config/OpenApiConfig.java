8a511106c14eb6c34c2b7296ef8c654bdbfb8b22
andregarcia0412 <andregarcia0412@gmail.com>
2026-09-21 14:14:20 -0300

    :wrench: chore: add bearer option to open api config

--- backend/src/main/java/me/andregarcia0412/pipeline/shared/config/OpenApiConfig.java
+++ backend/src/main/java/me/andregarcia0412/pipeline/shared/config/OpenApiConfig.java
@@ -1,14 +1,25 @@
 package me.andregarcia0412.pipeline.shared.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         info = @Info(
                 title="Platinado API",
                 version = "1.0.0"
-        )
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        description = "Access token returned by /auth/login, /auth/register or /auth/refresh"
 )
 @Configuration
 public class OpenApiConfig {
